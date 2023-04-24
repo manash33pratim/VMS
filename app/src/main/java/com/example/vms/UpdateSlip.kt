63b4dfc9ca240fefe80c436d6e36fbe2237d20bb
Manash Pratim Bezbaruah <94031744+manash33pratim@users.noreply.github.com>
2023-04-25 03:01:36 +0530

    no use

---
+++ app/src/main/java/com/example/vms/UpdateSlip.kt
@@ -0,0 +1,11 @@
+package com.example.vms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateSlip : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.update_slip)
+    }
+}
